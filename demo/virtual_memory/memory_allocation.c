afc4cacf099b6aa8924c194c65a455c4e90f648a
hunweitseng <h1tseng@cs.ucsd.edu>
2024-08-20 15:43:21 -0700

    VM Demo

---
+++ demo/virtual_memory/memory_allocation.c
@@ -0,0 +1,38 @@
+#define _GNU_SOURCE  
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <sched.h>
+#include <sys/syscall.h>
+#include <time.h>
+
+//#define dim 32768
+//#define dim 49152
+int main(int argc, char *argv[])
+{
+    int i,j;
+    double *a;
+    double sum=0, average, value;
+    int dim=32768;
+    if(argc < 2)
+    {
+        fprintf(stderr, "Usage: %s dimension\n",argv[0]);
+        exit(1);
+    }
+    dim = atoi(argv[1]);
+    value = atof(argv[2]);
+    a = (double *)malloc(sizeof(double)*dim*dim);
+//    for(i = 0 ; i < dim; i++)
+//        a[i] = (double *)malloc(sizeof(double)*dim);
+    for(i = 0 ; i < dim; i++)
+        for(j = 0 ; j < dim; j++)
+            a[i*dim+j] = value*i/(j+1);
+    for(i = 0 ; i < dim; i++)
+        for(j = 0 ; j < dim; j++)
+            sum+=a[i*dim+j];
+    average = sum/(dim*dim);
+    fprintf(stderr,"average: %lf\n",average);
+    free(a);
+    return 0;
+}
